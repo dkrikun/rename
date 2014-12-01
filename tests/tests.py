97f8bf7a1af26ec17800e8637641be79f6472d51
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-12-01 15:16:37 -0200

    add more docstrings on the tests

--- tests/tests.py
+++ tests/tests.py
@@ -9,7 +9,12 @@ class TestIsBinary(unittest.TestCase):
     binary_file_path = 'tests/tests_files/binary_file'
 
     def setUp(self):
+        "This will run before any test in this class"
         self.create_binary_file()
+
+    def tearDown(self):
+        "This will run after any test in this class"
+        self.remove_binary_file()
 
     def create_binary_file(self):
         "create a binary file to test with"
@@ -17,14 +22,11 @@ class TestIsBinary(unittest.TestCase):
         with open(self.binary_file_path, 'wb') as bf:
             bf.write(bytearray([1, 2, 3]))
 
-    def test_is_binary_should_return_true_if_binary_file(self):
-        "test if is_binary returns true when checking a binary file"
-        self.assertTrue(is_binary(self.binary_file_path))
-
-    def tearDown(self):
-        self.remove_binary_file()
-
     def remove_binary_file(self):
         "remove the binary file created for tests"
         import os
         os.remove(self.binary_file_path)
+
+    def test_is_binary_should_return_true_if_binary_file(self):
+        "test if is_binary returns true when checking a binary file"
+        self.assertTrue(is_binary(self.binary_file_path))
