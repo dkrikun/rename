81598dea847fb0e1a36eccd8ca2c2249768dc9f8
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-12-01 17:24:10 -0200

    Adding binaryornot as a dependency, so there is a requirements.txt now, and add it to setup.py. Also travis install it before running the tests

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ from setuptools import setup
 
 import rename
 
+requirements = [
+    'binaryornot>=0.3.0',
+]
 
 setup(
     name='rename',
@@ -18,4 +21,5 @@ setup(
     description='Rename a string in CamelCase, snake_case and ALL_CAPS in code and filenames in one go.',
     long_description=open('README.md').read(),
     scripts=['rename.py'],
+    install_requires=requirements,
 )
